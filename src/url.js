7510f3641ba263572e638fbfc4177c964dcca3dc
sayedmohamed <sayedmo.web.developer@gmail.com>
2026-01-19 19:30:51 +0200

    update server url

--- src/url.js
+++ src/url.js
@@ -1,2 +1,3 @@
 // export const url = 'https://social-media-app-backend-hsgm.onrender.com';
-export const url = 'http://localhost:8000';
+export const url = 'https://dark-space-backend.vercel.app/';
+// export const url = 'http://localhost:8000';
